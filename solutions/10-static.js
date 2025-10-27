2d3761ef35dcda0ecd9740747d9d8394a87f99b2
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-27 22:41:41 +0300

    Update 10-static.js

--- solutions/10-static.js
+++ solutions/10-static.js
@@ -1,6 +1,11 @@
 export default class Time {
   // BEGIN
-
+static fromString(timeAsString) {
+    const parts = timeAsString.split(':');
+    const hour = parseInt(parts[0], 10);
+    const minute = parseInt(parts[1], 10);
+    return new Time(hour, minute);
+  }
   // END
 
   constructor(hours, minutes) {
