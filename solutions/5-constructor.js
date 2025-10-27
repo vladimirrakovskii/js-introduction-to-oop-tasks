56f0cce7d571c2590fba2fd639779aee5ffdb0f3
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-27 22:37:21 +0300

    Update 5-constructor.js

--- solutions/5-constructor.js
+++ solutions/5-constructor.js
@@ -1,3 +1,21 @@
 // BEGIN
-
+export function Point(x, y) {
+    this.x = x;
+    this.y = y;
+    this.getX = () => this.x;
+    this.getY = () => this.y;
+}
+export function Segment(beginPoint, endPoint) {
+    this.beginPoint = beginPoint;
+    this.endPoint = endPoint;
+    this.getBeginPoint = () => this.beginPoint;
+    this.getEndPoint = () => this.endPoint;
+}
+export const reverse = (segment) => {
+    const beginPoint = segment.getBeginPoint();
+    const endPoint = segment.getEndPoint();
+    const reversedBeginPoint = new Point(endPoint.getX(), endPoint.getY());
+    const reversedEndPoint = new Point(beginPoint.getX(), beginPoint.getY());
+    return new Segment(reversedBeginPoint, reversedEndPoint);
+};
 // END
