47a562400c15dd879b7000bc1851a6e2dee028b0
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-27 22:41:12 +0300

    Update 9-class.js

--- solutions/9-class.js
+++ solutions/9-class.js
@@ -1,5 +1,21 @@
 import _ from 'lodash';
 
 // BEGIN
-
+export default class Cart {
+    constructor() {
+      this.items = [];
+    }
+    addItem(item, count) {
+      this.items.push({ item, count });
+    }
+    getItems() {
+      return _.cloneDeep(this.items);
+    }
+    getCost() {
+      return this.items.reduce((acc, { item, count }) => acc + item.price * count, 0);
+    }
+    getCount() {
+      return this.items.reduce((acc, { count }) => acc + count, 0);
+    }
+  }
 // END
