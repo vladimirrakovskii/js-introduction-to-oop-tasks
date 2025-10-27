9f9114b56f6b88992da8031d4699442bec8bd2a4
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-27 22:40:14 +0300

    Update 7-boxing.js

--- solutions/7-boxing.js
+++ solutions/7-boxing.js
@@ -1,3 +1,17 @@
 // BEGIN
-
+export default (...numbers) => {
+  const startSum = numbers.reduce((total, number) => total + number, 0);
+  const calculate = (...moreNumbers) => {
+    const currentSum = moreNumbers.reduce((total, number) => total + number, 0);
+    const totalSum = startSum + currentSum;
+    const nextCalculate = (...evenMoreNumbers) => {
+      const subsequentSum = evenMoreNumbers.reduce((total, number) => total + number, 0);
+      return calculate(currentSum + subsequentSum);
+    };
+    nextCalculate.valueOf = () => totalSum;
+    return nextCalculate;
+  };
+  calculate.valueOf = () => startSum;
+  return calculate;
+};
 // END
